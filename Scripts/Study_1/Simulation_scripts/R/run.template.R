f1fa8e414491ee6c5df6f98f3bf7f7d838027d21
Yuqi-psy <LiuYQ1119@163.com>
2024-10-17 17:22:46 +0200

    README file

--- Scripts/Study_1/Simulation_scripts/R/run.template.R
+++ Scripts/Study_1/Simulation_scripts/R/run.template.R
@@ -6,5 +6,5 @@ run.template <- function(template.path, envir, temp.filename.base) {
   brew(template, output=temp.filename, envir=envir) # writes to file
   close(template)
   
-  paste("C:\\Users\\liuy16\\Downloads\\LatentGOLD6.0\\lg60.exe", temp.filename, "/b") #write the correct location ofg LG
+  paste("~/lg60.exe", temp.filename, "/b") # specify the location of LatentGOLD
 } 
